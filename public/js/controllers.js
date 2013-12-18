bc6eabd61d37693322eb582861ba1488335cddc6
Denis Zaryaev <denis.zaryaev@gmail.com>
2013-12-19 00:58:04 +0200

    marks and summ of points

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -86,18 +86,65 @@ angular.module('myApp.controllers', [])
   $scope.studentsList = studentsList.data;
   
   $scope.calcScale = function(studentId, labId) {
-    var mark = ($scope.labMarks[studentId] || [])[labId] || 0;
+    var mark = ($scope.labMarks[studentId] || [])[labId];
     
-    if(mark < 0.3333)
+    if(!mark)
+      return '';
+    
+    if(mark < 61)
       return 'bad-mark';
     
-    if(mark < 0.6666)
+    if(mark < 75)
       return 'average-mark';
     
-    if(mark < 0.9)
+    if(mark < 90)
       return 'almost-good-mark';
     
     return 'good-mark';
+  }
+  
+  $scope.getTotalSumm = function() {
+    var summ = 0;
+    for(var i = 0; i < $scope.labsList.length; ++i)
+      summ += $scope.labsList[i].refMark;
+    
+    return summ;
+  }
+  
+  $scope.totalMarks = { };
+  $scope.getMark = function(studentId) {
+    if($scope.totalMarks[studentId] < 60)
+      return 'F';
+    
+    if($scope.totalMarks[studentId] <= 70)
+      return 'E';
+    
+    if($scope.totalMarks[studentId] <= 75)
+      return 'D';
+    
+    if($scope.totalMarks[studentId] <= 80)
+      return 'C';
+    
+    if($scope.totalMarks[studentId] < 90)
+      return 'B';
+    
+    return 'A';
+  }
+  
+  $scope.calcSum = function(studentId) {
+    var summ = 0;
+    var student = $scope.labMarks[studentId] || [];
+    
+    var ref = { };
+    $scope.labsList.map(function(v) {
+      ref[v.id] = v.refMark;
+    })
+    
+    for(var i in student)
+      summ += parseInt(student[i]) * ref[i] / 100;
+    
+    $scope.totalMarks[studentId] = summ;
+    return summ;
   }
   
   // mock data
@@ -108,19 +155,19 @@ angular.module('myApp.controllers', [])
   
   $scope.labMarks = {
     0: {
-      1: 0.5444444,
-      4: 1
+      1: 54,
+      4: 100
     },
     
     1: {
-      1: 0.5444444,
-      4: 1,
-      3: 0.8,
-      2: 0.9999999
+      1: 55,
+      4: 100,
+      3: 80,
+      2: 99
     },
     
     2: {
-      3: 0.1
+      3: 10
     }
   }
 }])
